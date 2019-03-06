fcdc85c2315ffa8560e87bc71136da999b3e5f01
feel1775 <44995013+feel1775@users.noreply.github.com>
2019-03-06 15:25:32 +0900

    Add files via upload

--- script12_ensemble.py
+++ script12_ensemble.py
@@ -23,20 +23,18 @@ knn = KNeighborsRegressor(n_neighbors=5)
 
 # usual knn
 knn.fit(xtrain,ytrain)
-knn.score(xtrain,ytrain)
-knn.score(xtest,ytest)
+print(knn.score(xtrain,ytrain),knn.score(xtest,ytest))
 
-# full bagging
+# full bagging n_estimators : 모델의 갯수, max_samples : resample의 비율, 0.5로 하면 반만 중복허용
+# max_features : feature를 뽑을 때 중복허용 비율 bootstrap : true(중복허용) bootstrap_features : true(중복허용)
 bf = BaggingRegressor(knn,n_estimators=100,max_samples=1.0,max_features=1.0,random_state=0)
 bf.fit(xtrain,ytrain)
-bf.score(xtrain,ytrain)
-bf.score(xtest,ytest)
+print(bf.score(xtrain,ytrain),bf.score(xtest,ytest))
 
 # bagging with subsampling and feature randomization
 bf = BaggingRegressor(knn,n_estimators=500,max_samples=0.5,max_features=0.5)
 bf.fit(xtrain,ytrain)
-bf.score(xtrain,ytrain)
-bf.score(xtest,ytest)
+print(bf.score(xtrain,ytrain),bf.score(xtest,ytest))
 
 # effect of estimators
 np.random.seed(0)
@@ -47,6 +45,7 @@ for i in range(len(n_list)):
     bf.fit(xtrain,ytrain)
     s[i,0] = bf.score(xtrain,ytrain)
     s[i,1] = bf.score(xtest,ytest)
+s
 plt.plot(np.log10(n_list),s,marker='o')
 
 # parameter tunning
@@ -59,8 +58,10 @@ for i in range(len(params)):
     f.fit(xtrain,ytrain)
     s[i,0] = f.score(xtrain,ytrain)
     s[i,1] = cross_val_score(f,xtrain,ytrain,cv=5).mean()
-#    s[i,1] = f.oob_score_
+#    s[i,1] = f.oob_score_   
+#oob_score가 더 빠른 출력 가능하다. CV하려면 많은 computing power 소모
     s[i,2] = f.score(xtest,ytest)
+s
 plt.plot(s)
 
 
@@ -73,30 +74,25 @@ from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor(n_estimators=100,random_state=0)
 rf.fit(xtrain,ytrain)
 yhat_test = rf.predict(xtest)
-rf.score(xtrain,ytrain)
-rf.score(xtest,ytest)
+print(rf.score(xtrain,ytrain),rf.score(xtest,ytest))
 
 # number of features to be chosen
 rf = RandomForestRegressor(n_estimators=100,random_state=0,max_features='sqrt')
 rf.fit(xtrain,ytrain)
-rf.score(xtrain,ytrain)
-rf.score(xtest,ytest)
+print(rf.score(xtrain,ytrain),rf.score(xtest,ytest))
 
 # tree size
 rf = RandomForestRegressor(n_estimators=100,random_state=0,
                            max_features='sqrt',max_leaf_nodes=100)
 rf.fit(xtrain,ytrain)
-rf.score(xtrain,ytrain)
-rf.score(xtest,ytest)
+print(rf.score(xtrain,ytrain),rf.score(xtest,ytest))
 
 # oob score
 rf = RandomForestRegressor(n_estimators=100,random_state=0,
                            max_features='sqrt',max_leaf_nodes=100,
                            oob_score=True)
 rf.fit(xtrain,ytrain)
-rf.score(xtrain,ytrain)
-rf.score(xtest,ytest)
-rf.oob_score_  # out-of-bag score
+print(rf.score(xtrain,ytrain),rf.score(xtest,ytest),rf.oob_score_)  # out-of-bag score
 
 # parameter tuning using oob scores
 tree_size = np.arange(2,50,2)
@@ -123,8 +119,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 gb = GradientBoostingRegressor(learning_rate=0.1,n_estimators=100,max_depth=3)
 gb.fit(xtrain,ytrain)
 yhat_test = gb.predict(xtest)
-gb.score(xtrain,ytrain)
-gb.score(xtest,ytest)
+print(gb.score(xtrain,ytrain),gb.score(xtest,ytest))
 
 # number of estimators
 n_list = np.arange(2,200,2)
@@ -132,6 +127,8 @@ s = np.zeros((len(n_list),3))
 for i in range(len(n_list)):
     gb = GradientBoostingRegressor(learning_rate=0.1,
                                    n_estimators=n_list[i],max_depth=3)
+    #learning rate : 람다 shrunkage index
+    
     gb.fit(xtrain,ytrain)
     s[i,0] = gb.score(xtrain,ytrain)
     s[i,1] = cross_val_score(gb,xtrain,ytrain,cv=5).mean() 
